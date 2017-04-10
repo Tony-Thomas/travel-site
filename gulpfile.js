429d6a30b6d10fb5c0acd46628feac5a82b09c6d
Tony <toekneethomas@gmail.com>
2017-04-10 13:43:33 -0700

    finish lesson 4 - Video 9

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'), 
+watch = require('gulp-watch');
 
 gulp.task('default', function() {
 	console.log("Hooray - You created a Gulp task!");
@@ -9,8 +9,17 @@ gulp.task('html', function() {
 	console.log("Imagine something useful being done to your HTML here.");
 });
 
+gulp.task('styles', function() {
+	console.log("Imagine SASS or PostCSS task running here.");
+});
+
 gulp.task('watch', function(){
+
 	watch('./app/index.html', function() {
 		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/styles.css', function() {
+		gulp.start('styles');
 	});
 });
