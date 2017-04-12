c8d840085461694928dbf3537ba7a3c7aede8e70
Tony <toekneethomas@gmail.com>
2017-04-11 19:05:03 -0700

    organzing gulp files: moved gulp watch app into /gulp/tasks/watch.js and imported watch.js and styles.css into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,7 @@
-var gulp = require('gulp'), 
-watch = require('gulp-watch'),
-browserSync = require('browser-Sync');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-	console.log("Hooray - You created a Gulp task!");
-});
 
-gulp.task('html', function() {
-	console.log("Imagine something useful being done to your HTML here.");
-});
 
-gulp.task('watch', function() {
 
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
 
-	watch('./app/index.html', function() {
-		browserSync.reload();
-	});
-
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('cssInject');
-	});
-
-});
-
-gulp.task('cssInject', ['styles'], function() {
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(browserSync.stream());
-});
