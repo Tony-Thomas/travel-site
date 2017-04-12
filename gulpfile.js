b2d637c3d09b90864ba7ec5fea0fc6851aff04ef
Tony <toekneethomas@gmail.com>
2017-04-11 18:48:19 -0700

    Organizing out gulpfile: sperated CSS automation into /gulp/tasks/styles.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,5 @@
 var gulp = require('gulp'), 
 watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
 browserSync = require('browser-Sync');
 
 gulp.task('default', function() {
@@ -13,12 +8,6 @@ gulp.task('default', function() {
 
 gulp.task('html', function() {
 	console.log("Imagine something useful being done to your HTML here.");
-});
-
-gulp.task('styles', function() {
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
