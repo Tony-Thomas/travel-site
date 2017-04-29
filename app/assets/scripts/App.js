5709e557edc66aa90695acfb508c2997f38f9f2b
Tony Thomas <anthonyhthomas@gmail.com>
2017-04-29 16:55:27 -0700

    49. Building a Modal From Scratch (Part 2) 19:41  complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,11 +2,13 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from  './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "75%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 
 // var revealOnScroll = new RevealOnScroll(); // replaced with the two lines above
 
